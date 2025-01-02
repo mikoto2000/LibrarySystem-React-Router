ce8a75d007bbcfba62d6fbe2d5b7d08d95386b9e
mikoto2000 <mikoto2000@gmail.com>
2025-01-02 09:55:16 +0000

    BookMaster のスキーマを追加。

--- app/infra/db/schema.ts
+++ app/infra/db/schema.ts
@@ -1,6 +1,14 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { date, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const authorTable = pgTable("author", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
+});
+
+export const bookMasterTable = pgTable("bookMaster", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  isbn: varchar({ length: 16 }).notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  publicationDate: date().notNull(),
+  authorId: integer("author_id").references(() => authorTable.id)
 });
