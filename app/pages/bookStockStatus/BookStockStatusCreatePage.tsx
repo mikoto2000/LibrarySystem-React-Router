9fe6c999a952c00641e291fa0c945fd292582206
mikoto2000 <mikoto2000@gmail.com>
2025-01-08 04:30:19 +0000

    BookStockStatus の登録画面に name のフィールドがなかった問題を修正。

--- app/pages/bookStockStatus/BookStockStatusCreatePage.tsx
+++ app/pages/bookStockStatus/BookStockStatusCreatePage.tsx
@@ -4,6 +4,7 @@ export const BookStockStatusCreatePage = () => {
   return (
     <main>
       <Form method="post">
+        <label>Name: <input type="text" name="name" /></label>
         <button type="submit">登録</button>
       </Form>
       <Link to="../bookStockStatuses">一覧に戻る</Link>
