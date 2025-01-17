4c192a41bd36180be598493a94e795ffdc03cc8d
mikoto2000 <mikoto2000@gmail.com>
2025-01-17 07:09:03 +0000

    bookStockStatusCreate から infra を排除。

--- app/routes/bookStockStatus/bookStockStatusCreate.tsx
+++ app/routes/bookStockStatus/bookStockStatusCreate.tsx
@@ -1,17 +1,13 @@
 import type { Route } from "./+types/bookStockStatusCreate";
 import { BookStockStatusCreatePage } from "../../views/pages/bookStockStatus/BookStockStatusCreatePage";
-import { db } from "~/infra/db";
-import { bookStockStatusTable } from "~/infra/db/schema";
 import { redirect } from "react-router";
+import { createBookStockStatus } from "~/services/BookStockStatusService";
 
 export async function action({ request }: Route.ActionArgs) {
   const formData = await request.formData();
   const name = formData.get("name")?.toString()
   if (name) {
-    const bookStockStatus: typeof bookStockStatusTable.$inferInsert = {
-      name,
-    };
-    const insertResult = await db.insert(bookStockStatusTable).values(bookStockStatus).returning();
+    const insertResult = await createBookStockStatus([{ name }]);
 
     return redirect(`/bookStockStatuses/${insertResult[0].id}`);
   } else {
--- app/services/BookStockStatusService.tsx
+++ app/services/BookStockStatusService.tsx
@@ -5,3 +5,7 @@ import type { BookStockStatus } from "~/types";
 export const findAllBookStockStatus = async (): Promise<BookStockStatus[]> => {
   return await db.select().from(bookStockStatusTable);
 }
+
+export const createBookStockStatus = async (bookStockStatus: { name: string }[]): Promise<{ id: number, name: string }[]> => {
+  return await db.insert(bookStockStatusTable).values(bookStockStatus).returning();
+}
