c38ee4ab7b7b367fd1950bb900ab35bcdf1adee0
mikoto2000 <mikoto2000@gmail.com>
2025-01-17 07:10:52 +0000

    bookStockDelete から infra を排除。

--- app/routes/bookStockStatus/bookStockStatusDelete.tsx
+++ app/routes/bookStockStatus/bookStockStatusDelete.tsx
@@ -1,16 +1,12 @@
 import type { Route } from "./+types/bookStockStatusDelete";
-import { db } from "~/infra/db";
-import { bookStockStatusTable } from "~/infra/db/schema";
 
-import { eq } from "drizzle-orm";
-import type { BookStockStatus } from "~/types";
 import { redirect } from "react-router";
+import { deleteBookStockStatus } from "~/services/BookStockStatusService";
 
 export async function action({ params }: Route.ActionArgs) {
   const id = params.id;
   if (id) {
-    await db.delete(bookStockStatusTable)
-      .where(eq(bookStockStatusTable.id, Number(id)));
+    await deleteBookStockStatus(Number(id));
 
     return redirect(`/bookStockStatuss`);
   } else {
--- app/services/BookStockStatusService.tsx
+++ app/services/BookStockStatusService.tsx
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { db } from "~/infra/db";
 import { bookStockStatusTable } from "~/infra/db/schema";
 import type { BookStockStatus } from "~/types";
@@ -8,4 +9,9 @@ export const findAllBookStockStatus = async (): Promise<BookStockStatus[]> => {
 
 export const createBookStockStatus = async (bookStockStatus: { name: string }[]): Promise<{ id: number, name: string }[]> => {
   return await db.insert(bookStockStatusTable).values(bookStockStatus).returning();
+}
+
+export const deleteBookStockStatus = async (id: number) => {
+  await db.delete(bookStockStatusTable)
+    .where(eq(bookStockStatusTable.id, id));
 }
