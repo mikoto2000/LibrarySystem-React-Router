4d336a130ae36842954ec6cde41beea225e9d9be
mikoto2000 <mikoto2000@gmail.com>
2025-01-18 01:20:21 +0000

    フォーマット修正。

--- app/routes/lendingSet/lendingSetDetail.tsx
+++ app/routes/lendingSet/lendingSetDetail.tsx
@@ -7,54 +7,54 @@ import { eq } from "drizzle-orm";
 import type { LendingSet } from "~/views/types";
 
 export async function loader({ params }: Route.LoaderArgs) {
-	const id = params.id;
+  const id = params.id;
 
-	const selectResult = await db.select()
-		.from(lendingSetTable)
-		.innerJoin(lendingStatusTable, eq(lendingSetTable.lendingStatusId, lendingStatusTable.id))
-		.innerJoin(lendingSetToBookStockTable, eq(lendingSetTable.id, lendingSetToBookStockTable.lendingSetId))
-		.innerJoin(bookStockTable, eq(bookStockTable.id, lendingSetToBookStockTable.bookStockId))
-		.innerJoin(bookMasterTable, eq(bookStockTable.bookMasterId, bookMasterTable.id))
-		.where(eq(lendingSetTable.id, Number(id)));
+  const selectResult = await db.select()
+    .from(lendingSetTable)
+    .innerJoin(lendingStatusTable, eq(lendingSetTable.lendingStatusId, lendingStatusTable.id))
+    .innerJoin(lendingSetToBookStockTable, eq(lendingSetTable.id, lendingSetToBookStockTable.lendingSetId))
+    .innerJoin(bookStockTable, eq(bookStockTable.id, lendingSetToBookStockTable.bookStockId))
+    .innerJoin(bookMasterTable, eq(bookStockTable.bookMasterId, bookMasterTable.id))
+    .where(eq(lendingSetTable.id, Number(id)));
 
-	//const lendingSet = selectResult;
-	const lendingSet = selectResult.reduce((acumulator, currentValue) => {
-		acumulator.id = currentValue.lending_set.id;
-		acumulator.lendStartDate = currentValue.lending_set.lendStartDate;
-		acumulator.lendDeadlineDate = currentValue.lending_set.lendDeadlineDate;
-		acumulator.returnDate = currentValue.lending_set.returnDate ? currentValue.lending_set.returnDate : "";
-		acumulator.memo = currentValue.lending_set.memo ? currentValue.lending_set.memo : "";
-			acumulator.bookStocks.push({
-				id: currentValue.book_stock.id,
-				bookMaster: currentValue.bookMaster,
-				memo: currentValue.book_stock.memo ? currentValue.book_stock.memo : "",
+  //const lendingSet = selectResult;
+  const lendingSet = selectResult.reduce((acumulator, currentValue) => {
+    acumulator.id = currentValue.lending_set.id;
+    acumulator.lendStartDate = currentValue.lending_set.lendStartDate;
+    acumulator.lendDeadlineDate = currentValue.lending_set.lendDeadlineDate;
+    acumulator.returnDate = currentValue.lending_set.returnDate ? currentValue.lending_set.returnDate : "";
+    acumulator.memo = currentValue.lending_set.memo ? currentValue.lending_set.memo : "";
+    acumulator.bookStocks.push({
+      id: currentValue.book_stock.id,
+      bookMaster: currentValue.bookMaster,
+      memo: currentValue.book_stock.memo ? currentValue.book_stock.memo : "",
 
-			})
-		return acumulator;
-	},
-		{
-			id: 0,
-			lendingStatus: { id: 1, name: "貸出中" },
-			lendStartDate: "",
-			lendDeadlineDate: "",
-			returnDate: "",
-			bookStocks: [],
-			memo: "",
-		} as LendingSet);
+    })
+    return acumulator;
+  },
+    {
+      id: 0,
+      lendingStatus: { id: 1, name: "貸出中" },
+      lendStartDate: "",
+      lendDeadlineDate: "",
+      returnDate: "",
+      bookStocks: [],
+      memo: "",
+    } as LendingSet);
 
-	return { lendingSet };
+  return { lendingSet };
 }
 
 export function meta({ }: Route.MetaArgs) {
-	return [
-		{ title: "New React Router App" },
-		{ name: "description", content: "Welcome to React Router!" },
-	];
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
 }
 
 export default function LendingSetDetail({ loaderData }: Route.ComponentProps) {
-	return <LendingSetDetailPage
-		lendingSet={loaderData.lendingSet}
-	/>;
+  return <LendingSetDetailPage
+    lendingSet={loaderData.lendingSet}
+  />;
 }
 
