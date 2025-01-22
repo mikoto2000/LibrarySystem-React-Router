fa7240554fd440d48fe06394e644c4721999d23e
mikoto2000 <mikoto2000@gmail.com>
2025-01-22 12:23:39 +0000

    BookStockPage にスタイルを適用。

--- app/views/pages/bookStock/BookStockPage.tsx
+++ app/views/pages/bookStock/BookStockPage.tsx
@@ -1,29 +1,73 @@
 import { Link } from "react-router";
+import { Button } from "~/components/button/Button";
+import { Table } from "~/components/table/Table";
+import type { BookStockWithoutAuthor } from "~/views/types";
 
 type BookStockPageProps = {
-  bookStocks: any[],
+  bookStocks: BookStockWithoutAuthor[],
 }
 
 export const BookStockPage = ({ bookStocks }: BookStockPageProps) => {
   return (
     <main>
-      <h2>BookStocks</h2>
-      <Link to="create">新規登録</Link>
-      <ul>
-        {
-          bookStocks
-            ?
-            bookStocks.map((e) => <li><Link to={`/bookStocks/${e.id}`}>{e.id}: {e.bookMaster.name}, {e.bookStockStatus.name}</Link></li>)
-            :
-            <>表示要素がありませんでした</>
-        }
-      </ul>
-      <Link to="/">トップに戻る</Link>
-      <pre>
-        {
-          JSON.stringify(bookStocks, null, 2)
-        }
-      </pre>
+      <div className="pb-3">
+        <h2 className="font-bold text-2xl mt-2 mb-1 ">BookStocks</h2>
+        <Button
+          label="新規作成"
+          to="create"
+        />
+      </div>
+      <Table<any>
+        linkTo="lendingStatuses"
+        headerInfo={[
+          {
+            name: "Id",
+            onClick: () => { },
+          },
+          {
+            name: "Name",
+            onClick: () => { },
+          },
+          {
+            name: "Status",
+            onClick: () => { },
+          },
+          {
+            name: "Memo",
+            onClick: () => { },
+          },
+        ]}
+        contentInfo={[
+          {
+            getValueFunc: (e) => e.id.toString()
+          },
+          {
+            getValueFunc: (e) => e.bookMaster.name
+          },
+          {
+            getValueFunc: (e) => e.bookStockStatus.name
+          },
+          {
+            getValueFunc: (e) => e.memo
+          },
+        ]}
+        content={bookStocks}
+      ></Table>
+      <Link
+        className="text-blue-600 dark:text-blue-500 hover:underline"
+        to="/"
+      >トップに戻る</Link>
+      {
+        import.meta.env.DEV
+          ?
+          <pre>
+            {
+              JSON.stringify(bookStocks, null, 2)
+            }
+          </pre>
+          :
+          <></>
+      }
     </main>
   )
 }
