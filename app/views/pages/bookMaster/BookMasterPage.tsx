05ab63506a6ac7a8f23ef2e596198298ac5763c9
mikoto2000 <mikoto2000@gmail.com>
2025-01-22 12:17:44 +0000

    BookMasterPage にスタイルを適用。

--- app/views/pages/bookMaster/BookMasterPage.tsx
+++ app/views/pages/bookMaster/BookMasterPage.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router";
-import type { BookMasterList } from "~/views/types";
+import { Button } from "~/components/button/Button";
+import { Table } from "~/components/table/Table";
+import type { BookMasterList, BookMasterWithoutAuthors } from "~/views/types";
 
 type BookMasterPageProps = {
   bookMasters: BookMasterList,
@@ -8,23 +10,64 @@ type BookMasterPageProps = {
 export const BookMasterPage = ({ bookMasters }: BookMasterPageProps) => {
   return (
     <main>
-      <h2>BookMasters</h2>
-      <Link to="create">新規登録</Link>
-      <ul>
-        {
-          bookMasters
-            ?
-            bookMasters.map((e) => <li><Link to={`/bookMasters/${e.id}`}>{e.id}: {e.name}</Link></li>)
-            :
-            <>表示要素がありませんでした</>
-        }
-      </ul>
-      <Link to="/">トップに戻る</Link>
-      <pre>
-        {
-          JSON.stringify(bookMasters, null, 2)
-        }
-      </pre>
+      <div className="pb-3">
+        <h2 className="font-bold text-2xl mt-2 mb-1 ">BookMasters</h2>
+        <Button
+          label="新規作成"
+          to="create"
+        />
+      </div>
+      <Table<BookMasterWithoutAuthors>
+        linkTo="lendingStatuses"
+        headerInfo={[
+          {
+            name: "Id",
+            onClick: () => { },
+          },
+          {
+            name: "ISBN",
+            onClick: () => { },
+          },
+          {
+            name: "Book Name",
+            onClick: () => { },
+          },
+          {
+            name: "Publication Date",
+            onClick: () => { },
+          },
+        ]}
+        contentInfo={[
+          {
+            getValueFunc: (e) => e.id.toString()
+          },
+          {
+            getValueFunc: (e) => e.isbn
+          },
+          {
+            getValueFunc: (e) => e.name
+          },
+          {
+            getValueFunc: (e) => e.publicationDate
+          },
+        ]}
+        content={bookMasters}
+      ></Table>
+      <Link
+        className="text-blue-600 dark:text-blue-500 hover:underline"
+        to="/"
+      >トップに戻る</Link>
+      {
+        import.meta.env.DEV
+          ?
+          <pre>
+            {
+              JSON.stringify(bookMasters, null, 2)
+            }
+          </pre>
+          :
+          <></>
+      }
     </main>
   )
 }
