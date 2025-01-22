7624a64aa6668ca06ee4f4fb132e8386b54d77af
mikoto2000 <mikoto2000@gmail.com>
2025-01-22 12:10:29 +0000

    CustomerPage にスタイルを適用。

--- app/views/pages/customer/CustomerPage.tsx
+++ app/views/pages/customer/CustomerPage.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router";
+import { Button } from "~/components/button/Button";
+import { Table } from "~/components/table/Table";
 import type { Customer } from "~/types";
 
 type CustomerPageProps = {
@@ -8,18 +10,47 @@ type CustomerPageProps = {
 export const CustomerPage = ({ customeres }: CustomerPageProps) => {
   return (
     <main>
-      <h2>Customeres</h2>
-      <Link to="create">新規登録</Link>
-      <ul>
-        {
-          customeres
-            ?
-            customeres.map((e) => <li><Link to={`/customers/${e.id}`}>{e.id}: {e.name}</Link></li>)
-            :
-            <>表示要素がありませんでした</>
-        }
-      </ul>
+      <div className="pb-3">
+        <h2 className="font-bold text-2xl mt-2 mb-1 ">Customeres</h2>
+        <Button
+          label="新規作成"
+          to="create"
+        />
+      </div>
+      <Table<Customer>
+        linkTo="customers"
+        headerInfo={[
+          {
+            name: "Id",
+            onClick: () => { },
+          },
+          {
+            name: "Name",
+            onClick: () => { },
+          },
+        ]}
+        contentInfo={[
+          {
+            getValueFunc: (e) => e.id.toString()
+          },
+          {
+            getValueFunc: (e) => e.name
+          },
+        ]}
+        content={customeres}
+      ></Table>
       <Link to="/">トップに戻る</Link>
+      {
+        import.meta.env.DEV
+          ?
+          <pre>
+            {
+              JSON.stringify(customeres, null, 2)
+            }
+          </pre>
+          :
+          <></>
+      }
     </main>
   )
 }
