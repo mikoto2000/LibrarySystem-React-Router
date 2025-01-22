c6e96d7005ca40f375948ea7547d55524f3a52ba
mikoto2000 <mikoto2000@gmail.com>
2025-01-22 12:43:19 +0000

    LendingSetPage にスタイルを追加。

--- app/views/pages/lendingSet/LendingSetPage.tsx
+++ app/views/pages/lendingSet/LendingSetPage.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router";
-import type { LendingSetList } from "~/views/types";
+import { Button } from "~/components/button/Button";
+import { Table } from "~/components/table/Table";
+import type { LendingSetList, LendingSetWithoutAuthor } from "~/views/types";
 
 type LendingSetPageProps = {
   lendingSets: LendingSetList,
@@ -8,23 +10,92 @@ type LendingSetPageProps = {
 export const LendingSetPage = ({ lendingSets }: LendingSetPageProps) => {
   return (
     <main>
-      <h2>LendingSets</h2>
-      <Link to="create">新規登録</Link>
-      <ul>
-        {
-          lendingSets
-            ?
-            lendingSets.map((e) => <li><Link to={`/lendingSets/${e.id}`}>{e.id}: [{e.lendingStatus.name}] {e.customer}: Books[{e.bookStocks.toString()}</Link></li>)
-            :
-            <>表示要素がありませんでした</>
-        }
-      </ul>
-      <Link to="/">トップに戻る</Link>
-      <pre>
-        {
-          JSON.stringify(lendingSets, null, 2)
-        }
-      </pre>
+      <div className="pb-3">
+        <h2 className="font-bold text-2xl mt-2 mb-1 ">LendingSets</h2>
+        <Button
+          label="新規作成"
+          to="create"
+        />
+      </div>
+      <Table<LendingSetWithoutAuthor>
+        linkTo="lendingStatuses"
+        headerInfo={[
+          {
+            name: "Id",
+            onClick: () => { },
+          },
+          {
+            name: "Status",
+            onClick: () => { },
+          },
+          {
+            name: "Customer",
+            onClick: () => { },
+          },
+          {
+            name: "Lend Start Date",
+            onClick: () => { },
+          },
+          {
+            name: "Lend Deadline Date",
+            onClick: () => { },
+          },
+          {
+            name: "Return Date",
+            onClick: () => { },
+          },
+          {
+            name: "Books",
+            onClick: () => { },
+          },
+          {
+            name: "Memo",
+            onClick: () => { },
+          },
+        ]}
+        contentInfo={[
+          {
+            getValueFunc: (e) => e.id.toString()
+          },
+          {
+            getValueFunc: (e) => e.lendingStatus.name
+          },
+          {
+            getValueFunc: (e) => e.customer
+          },
+          {
+            getValueFunc: (e) => e.lendStartDate
+          },
+          {
+            getValueFunc: (e) => e.lendDeadlineDate
+          },
+          {
+            getValueFunc: (e) => e.returnDate
+          },
+          {
+            getValueFunc: (e) => e.bookStocks.join(", ")
+          },
+          {
+            getValueFunc: (e) => e.memo ? e.memo : ""
+          },
+        ]}
+        content={lendingSets}
+      ></Table>
+      <Link
+        className="text-blue-600 dark:text-blue-500 hover:underline"
+        to="/"
+      >トップに戻る</Link>
+      {
+        import.meta.env.DEV
+          ?
+          <pre>
+            {
+              JSON.stringify(lendingSets, null, 2)
+            }
+          </pre>
+          :
+          <></>
+      }
     </main>
   )
 }
