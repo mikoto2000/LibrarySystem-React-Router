fe93d3d6efedc090c487dd05f7d536ceda72faa3
mikoto2000 <mikoto2000@gmail.com>
2025-01-25 04:02:07 +0000

    LendingSetDetailPage にスタイルを適用。

--- app/views/pages/lendingSet/LendingSetDetailPage.tsx
+++ app/views/pages/lendingSet/LendingSetDetailPage.tsx
@@ -1,4 +1,6 @@
-import { Form, Link } from "react-router";
+import { Form } from "react-router";
+import { DetailView } from "~/components/detailview/DetailView";
+import { Link } from "~/components/link/Link";
 import { LinkButton } from "~/components/linkbutton/LinkButton";
 import { SubmitButton } from "~/components/submitbutton/SubmitButton";
 import type { LendingSet } from "~/views/types";
@@ -10,13 +12,47 @@ type LendingSetDetailPageProps = {
 export const LendingSetDetailPage = ({ lendingSet }: LendingSetDetailPageProps) => {
   return (
     <main>
-      <pre>
-        {
-          JSON.stringify(lendingSet, null, 2)
-        }
-      </pre>
+      <h2 className="font-bold text-2xl mt-2 mb-1 ">LendingSet</h2>
+      <DetailView<LendingSet>
+        content={lendingSet}
+        valueInfos={[
+          {
+            name: "Id",
+            getValueFunc: (e) => e.id.toString()
+          },
+          {
+            name: "Status",
+            getValueFunc: (e) => e.lendingStatus.name
+          },
+          {
+            name: "Books",
+            getValueFunc: (e) => e.bookStocks.map((b) => {
+              return b.id + ": " + b.bookMaster.name
+              }).join(", ")
+          },
+          {
+            name: "Lend Start Date",
+            getValueFunc: (e) => e.lendStartDate
+          },
+          {
+            name: "Lend Deadline Date",
+            getValueFunc: (e) => e.lendDeadlineDate
+          },
+          {
+            name: "Return Date",
+            getValueFunc: (e) => e.returnDate ? e.returnDate : ""
+          },
+          {
+            name: "Memo",
+            getValueFunc: (e) => e.memo ? e.memo : ""
+          },
+        ]}
+      />
       <div className="pl-1 pt-3 pb-1">
-        <LinkButton label="編集する" to={`/lendingSets/${lendingSet.id}/edit`} />
+        <LinkButton
+          label="編集する"
+          to={`/lendingSets/${lendingSet.id}/edit`}
+        />
         {" "}
         <Form className="inline" method="post" action={`/lendingSets/${lendingSet.id}/delete`}>
           <SubmitButton
@@ -24,10 +60,24 @@ export const LendingSetDetailPage = ({ lendingSet }: LendingSetDetailPageProps) 
           />
         </Form>
       </div>
-      {" "}
-      <Link to="/lendingSets">一覧へ戻る</Link>
-      {" "}
-      <Link to="/">トップへ戻る</Link>
+      <div>
+        <Link
+          label="一覧へ戻る"
+          to="/lendingSets" />
+        {" "}
+        <Link
+          label="トップへ戻る"
+          to="/" />
+      </div>
+      <pre>
+        {
+          import.meta.env.DEV
+            ?
+            JSON.stringify(lendingSet, null, 2)
+            :
+            <></>
+        }
+      </pre>
     </main>
   )
 }
