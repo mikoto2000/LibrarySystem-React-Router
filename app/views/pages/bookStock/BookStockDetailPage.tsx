02accc1563519f4f5511d81cfe68d2265cf6b602
mikoto2000 <mikoto2000@gmail.com>
2025-01-25 03:11:04 +0000

    BookStockDetailPage にスタイルを適用。

--- app/views/pages/bookStock/BookStockDetailPage.tsx
+++ app/views/pages/bookStock/BookStockDetailPage.tsx
@@ -1,4 +1,6 @@
-import { Form, Link } from "react-router";
+import { Form } from "react-router";
+import { DetailView } from "~/components/detailview/DetailView";
+import { Link } from "~/components/link/Link";
 import { LinkButton } from "~/components/linkbutton/LinkButton";
 import { SubmitButton } from "~/components/submitbutton/SubmitButton"
 import type { BookStockWithoutAuthor } from "~/views/types";
@@ -10,12 +12,27 @@ type BookStockDetailPageProps = {
 export const BookStockDetailPage = ({ bookStock }: BookStockDetailPageProps) => {
   return (
     <main>
-      <ul>
-        <li>Id: {bookStock.id}</li>
-        <li>Book Name: {bookStock.bookMaster.name}</li>
-        <li>Status: {bookStock.bookStockStatus.name}</li>
-        <li>Memo: {bookStock.memo}</li>
-      </ul>
+      <DetailView<BookStockWithoutAuthor>
+        content={bookStock}
+        valueInfos={[
+          {
+            name: "Id",
+            getValueFunc: (e) => e.id.toString()
+          },
+          {
+            name: "Book Name",
+            getValueFunc: (e) => e.bookMaster.name
+          },
+          {
+            name: "Status",
+            getValueFunc: (e) => e.bookStockStatus.name
+          },
+          {
+            name: "Memo",
+            getValueFunc: (e) => e.memo
+          },
+        ]}
+      />
       <div className="pl-1 pt-3 pb-1">
         <LinkButton label="編集する" to={`/bookStocks/${bookStock.id}/edit`} />
         {" "}
@@ -26,12 +43,22 @@ export const BookStockDetailPage = ({ bookStock }: BookStockDetailPageProps) => 
         </Form>
       </div>
       {" "}
-      <Link to="/bookStocks">一覧へ戻る</Link>
-      {" "}
-      <Link to="/">トップへ戻る</Link>
+      <div>
+        <Link
+          label="一覧へ戻る"
+          to="/authors" />
+        {" "}
+        <Link
+          label="トップへ戻る"
+          to="/" />
+      </div>
       <pre>
         {
-          JSON.stringify(bookStock, null, 2)
+          import.meta.env.DEV
+            ?
+            JSON.stringify(bookStock, null, 2)
+            :
+            <></>
         }
       </pre>
     </main>
